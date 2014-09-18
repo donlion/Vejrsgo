d8b06c39b492060ef0f2b558aba67fb64b222604
Leo Ørsnes <zeekooster@gmail.com>
2014-09-19 01:49:51 +0200

    Interactions and animations mainly

--- src/js/vejrsgo.js
+++ src/js/vejrsgo.js
@@ -3,11 +3,39 @@ var Vejrsgo = new function() {
 	this.WeatherAPIKey = "e494905c60db134c0b21009c27b069d67d81e8a0";
 	
 	this.setup = function() {
-		this.showCards();
 		this.weather();
+		this.setupBindings();
+	};
+
+	this.setupBindings = function() {
+		this.Ripple();
+
+		$(".card").click(function() {
+			$(".card.highlight").each(function() {
+					$(this).css("top", "-"+$(this).outerHeight()+"px").css("opacity", "0");
+					var card = $(this);
+					setTimeout(function() {
+						card.remove();
+					}, 2000);
+			});
+			var newCard = $(this).clone();
+			newCard.addClass("highlight");
+			newCard.find(".ripple").remove();
+			newCard.find(".sun").addClass("sun--big");
+			$("body").append(newCard);
+
+			newCard.click(function() {
+				$(this).css("top", "-"+$(this).outerHeight()+"px").css("opacity", "0");
+				var card = $(this);
+				setTimeout(function() {
+					card.remove();
+				}, 2000);
+			});
+		});
 	};
 
 	this.showCards = function() {
+		Vejrsgo.load.hide();
 		var cardDelay = 200;
 		var cardDelayFactor = 1.5;
 		$(".card").each(function() {
@@ -24,7 +52,7 @@ var Vejrsgo = new function() {
 	};
 
 
-	this.getWeather = function(card, city) {
+	this.getWeather = function(card, city, callback) {
 		console.log("getWeather initiated");
 		city = city.replace(" ", "+");
 
@@ -41,7 +69,7 @@ var Vejrsgo = new function() {
 			contentType: "JSON",
 			dataType: "JSON",
 		}).done(function (data) {
-			Vejrsgo.injectWeather(card, city, data.data);
+			Vejrsgo.injectWeather(card, city, data.data, callback);
 		}).fail(function(error, text) {
 			console.log("Something went wrong");
 			return false;
@@ -51,19 +79,21 @@ var Vejrsgo = new function() {
 	this.weather = function() {
 		console.log("weather initiated");
 
+		Vejrsgo.load.show();
+
 		var filter = $("[data-city]");
 
 		filter.each(function() {
 
 			var	card = $(this),
 					city = card.attr("data-city");
-			Vejrsgo.getWeather(card, city);
+			Vejrsgo.getWeather(card, city, Vejrsgo.showCards);
 
 		});
 
 	};
 
-	this.injectWeather = function(card, city, data) {
+	this.injectWeather = function(card, city, data, callback) {
 		console.log(data["current_condition"][0]["temp_C"]);
 		if (!data) {
 			card.css("opacity", "0.4");
@@ -95,11 +125,70 @@ var Vejrsgo = new function() {
 
 			});
 
+			callback();
+
 		}
 	};
 
 	this.scopes = {
 
 	};
+
+	this.load = {
+
+    loadElement: $(".load.load--container"),
+
+    show: function() {
+      $(".load.load--container").css("opacity", "1");
+    },
+
+    hide: function(el) {
+      $(".load.load--container").css("opacity", "0");
+    }
+
+	};
+
+
+  this.Ripple = function()  {
+      $("[data-ripple]").each(function() {
+          if ($(this).css("overflow") != "hidden") {
+              $(this).css("overflow", "hidden");
+          }
+          if ($(this).css("position") != "relative") {
+              $(this).css("position", "relative");
+          }
+      });
+
+      $("[data-ripple]").on("mousedown", function(e) {
+          if ($(this).find(".ripple").length != 0) {
+              $(this).find(".ripple").remove();
+          }
+          var parent = $(this).parent();
+
+          $(this).prepend("<span class='ripple' />");
+
+          var ripple = $(this).find(".ripple");
+
+          if (!ripple.height() || !ripple.width()) {
+              var d = Math.max(parent.outerWidth(), parent.outerHeight());
+              ripple.css({
+                  height: d,
+                  width: d
+              });
+          }
+
+          var x = e.pageX - $(this).offset().left - ripple.width() / 2;
+          var y = e.pageY - $(this).offset().top - ripple.height() / 2;
+
+          ripple.css({
+              top: y + 'px',
+              left: x + 'px'
+          }).addClass("animate");
+
+          setTimeout(function() {
+              ripple.remove();
+          }, 1000);
+      });
+  };
 
 };
