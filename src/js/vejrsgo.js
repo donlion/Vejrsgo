b980b5ec26ae1178282a83a35cf6f3d5d3bb3cd4
Leo Ørsnes <zeekooster@gmail.com>
2014-10-02 17:43:32 +0200

    Clean-up, overlay and optimisation

--- src/js/vejrsgo.js
+++ src/js/vejrsgo.js
@@ -3,9 +3,11 @@ var Vejrsgo = new function() {
 	this.WeatherAPIKey = "e494905c60db134c0b21009c27b069d67d81e8a0";
 	
 	this.setup = function() {
-		this.weather();
+		//this.weather();
 		this.setupBindings();
 		this.attachFooter();
+		this.attachOverlay();
+		this.system();
 	};
 
 	this.setupBindings = function() {
@@ -32,6 +34,22 @@ var Vejrsgo = new function() {
 					card.remove();
 				}, 2000);
 			});
+		});
+
+		$("body").on("click", "a", function(e) {
+
+			if ($(this).data("action") == undefined) {
+				return;
+			}
+
+			e.preventDefault();
+
+			var action = $(this).data("action");
+
+			if (action == "login") {
+				console.log("log ind pressed");
+			}
+
 		});
 	};
 
@@ -203,6 +221,81 @@ var Vejrsgo = new function() {
   	}
 
 
+
+  };
+
+  this.attachOverlay = function() {
+
+  	console.log("overlays attached");
+
+  	$("footer a.overlay").click(function(e) {
+  		console.log("footer overlay inititated");
+  		e.preventDefault();
+
+  		Vejrsgo.overlay("privacypolicy");
+  	});
+  };
+
+  this.overlay = function(arg) {
+
+  	if (!arg) {
+  		return;
+  	}
+
+  	if (arg == "privacypolicy") {
+
+  		var text = "Her kommer vores privatlivspolitik....<br />";
+  		text += "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur molestias tempore, neque repellat et. Perferendis ea cum, sit numquam quidem sed nihil laboriosam autem exercitationem quae error et amet officia.";
+  		text += "</p><p class='overlay__card__paragraph'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi, deleniti, ad voluptatum eum voluptatibus illo perferendis minima doloremque. Aliquid laboriosam aspernatur cumque vitae, architecto, labore magnam a? Placeat, maxime ducimus."
+
+  		var output = [
+  		"<div class='overlay'>",
+  		"<div class='overlay__card'>",
+  		"<h3 class='overlay__card__header'>Privat politik</h3>",
+  		"<p class='overlay__card__paragraph'>",
+  		text,
+  		"</p>",
+  		"</div>",
+  		"</div>"
+  		];
+
+  		console.log("output array: "+output.join(", "));
+
+  		$("body").append(output.join(""));
+
+  		setTimeout(function() {
+  			$("div.overlay").addClass("shown");
+  		}, 200);
+
+  	}
+
+  };
+
+  this.storage = {
+
+  	set: function(arg, val) {
+  		localStorage.setItem(arg, val);
+  	},
+
+  	get: function(arg) {
+  		return localStorage.getItem(arg);
+  	}
+
+  };
+
+
+  this.system = function() {
+
+  	if (Vejrsgo.storage.get("user")) {
+  		
+  		console.log(Vejrsgo.storage.get("user"));
+
+  	} else {
+
+  		console.log("NOT SET!");
+  		Vejrsgo.storage.set("user", "Leo Ørsnes");
+
+  	}
 
   };
 
